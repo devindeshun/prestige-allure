26dc4dd33e19bb62f4cb09abf8abf7dba841b8e6
devindshun <devin.dshun@gmail.com>
2020-05-09 23:56:01 -0500

    Added a exit intent popup to make the store look more like a modern e-commerce store. Only works in desktop mode.

--- js/code.js
+++ js/code.js
@@ -1,4 +1,4 @@
-// When the user scrolls the page, execute myFunction
+// When the user scrolls the page, execute stickyNav
 window.onscroll = function() {
   stickyNav();
 };
@@ -11,3 +11,20 @@ let sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 let stickyNav = () => window.pageYOffset > sticky ? header.classList.add("sticky") : header.classList.remove("sticky");
+
+function mouseOut(event) {
+  if (event.clientY < 20) {
+    document.removeEventListener("mouseout", mouseOut);
+    document.getElementById("popup").style.display = "block";
+  }
+}
+
+function closePopup() {
+  if (document.getElementById("popup").style.display == "block") {
+    document.removeEventListener("click", closePopup);
+    document.getElementById("popup").style.display = "none";
+  }
+}
+
+document.addEventListener("mouseout", mouseOut);
+document.addEventListener("click", closePopup);
